21bf7ce4bf9b776fe22d2431bc1b057da09dfbf8
RAArrieta <alfredoarrieta99@gmail.com>
2024-05-14 17:31:31 -0300

    creo modelos usuario y cargo y los registro

--- my_App/usuario/models.py
+++ my_App/usuario/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class UsuarioCargo(models.Model):
+    nombre = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f"{self.nombre}"
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    nacimiento = models.DateField(null=True)
+    telefono = models.CharField(max_length=50)
+    mail = models.CharField(max_length=50)   
+    cargo_id = models.ForeignKey(UsuarioCargo, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.cargo_id}: {self.apellido}, {self.nombre}"
