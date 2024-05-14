39dabbd12a65219cb2cd8d5a97a1b8b1cfbffefe
RAArrieta <alfredoarrieta99@gmail.com>
2024-05-14 17:10:29 -0300

    creo un template y lo configuro

--- my_App/project/urls.py
+++ my_App/project/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("core.urls")),
     path('producto/', include("producto.urls")),
+    path('usuario/', include("usuario.urls")),
 ]
