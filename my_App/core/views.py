c0ef904377f56fb6f8d553b9521b116772963d87
RAArrieta <alfredoarrieta99@gmail.com>
2024-05-15 22:06:26 -0300

    login, logout

--- my_App/core/views.py
+++ my_App/core/views.py
@@ -1,4 +1,11 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from .forms import CustomAuthenticationForm
 
 def home (request):
     return render(request, "core/index.html")
+
+class CustomLoginView(LoginView):
+    authentication_form = CustomAuthenticationForm
+    template_name = "core/login.html"
+
