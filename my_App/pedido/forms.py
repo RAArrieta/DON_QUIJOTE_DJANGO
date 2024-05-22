c3d0f6dcb9ca40f5cdf578b91b88b368c402644f
RAArrieta <alfredoarrieta99@gmail.com>
2024-05-22 14:07:26 -0300

    Reestructuracion 1

--- my_App/pedido/forms.py
+++ my_App/pedido/forms.py
@@ -6,10 +6,23 @@ class PedidoForm(forms.ModelForm):
     class Meta:
         model = Pedido
         fields = "__all__"
+        widgets = {
+            "estado": forms.Select(attrs={"class": "form-control"}),
+            "hora": forms.TimeInput(attrs={"class": "form-control"}),
+            "direccion": forms.TextInput(attrs={"class": "form-control"}),
+            "cliente": forms.TextInput(attrs={"class": "form-control"}),
+            "observacion": forms.TextInput(attrs={"class": "form-control"}),
+            "precio_total": forms.NumberInput(attrs={"class": "form-control"}),
+            "pago": forms.Select(attrs={"class": "form-control"}),
+        }
 
 class PedidoProductoForm(forms.ModelForm):
     class Meta:
         model = PedidoProducto
         fields = ['producto', 'cantidad']
+        widgets = {
+            "producto": forms.Select(attrs={"class": "form-control"}),
+            "cantidad": forms.NumberInput(attrs={"class": "form-control"}),
+        }
 
 PedidoProductoFormSet = inlineformset_factory(Pedido, PedidoProducto, form=PedidoProductoForm, extra=1, can_delete=True)
